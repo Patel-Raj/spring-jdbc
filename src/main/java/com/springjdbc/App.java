b79c48e598f8ea5ca698d0dc2ae9d91cadb48899
Raj Patel <patelraj22777@gmail.com>
2022-01-04 13:58:19 +0530

    Inserting data

--- src/main/java/com/springjdbc/App.java
+++ src/main/java/com/springjdbc/App.java
@@ -1,13 +1,21 @@
 package com.springjdbc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
+        JdbcTemplate jdbcTemplate = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
+        String query = "insert into student(id,name,city) values(?,?,?)";
+        int res = jdbcTemplate.update(query, 11,"Manan","Kanpur");
+        System.out.println(res);
     }
+
 }
